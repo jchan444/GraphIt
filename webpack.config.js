d2b48bbeccb06649b10e4d56a150fb731aea4c75
Johnny Chan <104582725+jchan444@users.noreply.github.com>
2022-06-21 14:11:10 -0700

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const nodeEnv = process.env.NODE_ENV;
+
+module.exports = {
+    entry: './client/index.js',
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'bundle.js',
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './index.html'
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test:/\.jsx?/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react']
+                    }
+                }
+            }
+        ]
+    },
+
+    mode: nodeEnv,
+}
